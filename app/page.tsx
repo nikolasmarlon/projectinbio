077796d9574c6db4d46c720ffad2be82236bb99e
Nikolas <nikmarlon.santos@gmail.com>
2025-04-08 00:21:57 -0300

    erro css tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="background-primary">Helo world</h1>
+      <h1>Helo world</h1>
     </div>
   );
 }
